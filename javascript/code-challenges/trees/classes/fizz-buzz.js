4415400fcba734c51557086415ff7640fbcf3114
Justin Hamerly <suvimountain@icloud.com>
2022-10-13 02:39:23 -0700

    fizzbuzz tree refactor

--- javascript/code-challenges/trees/classes/fizz-buzz.js
+++ javascript/code-challenges/trees/classes/fizz-buzz.js
@@ -2,40 +2,33 @@ const Exception = require('./exception');
 const Node = require('./knode');
 const Ktree = require('./karytree');
 
-module.exports = function fizzbuzzTree(tree){
-
-  if(!tree.root){
+module.exports = function fizzbuzzTree(tree) {
+  if (!tree.root) {
     throw new Exception('No nodes in tree');
   }
-
   let newTree = new Ktree;
-
-  newTree.root = new Node(tree.root.value);
-
-  replaceValues(tree.root, newTree.root);
-
+  newTree.root = new Node(changeValueFB(tree.root.value));
+  createNewTree(tree.root, newTree.root);
   return newTree;
-
 };
 
-function replaceValues(oldnode, newnode){
-  if(oldnode.child.length){
-    for (let i=0; i<oldnode.child.length; i++){
-      let newChild = new Node(oldnode.child[i].value);
-      newnode.child.push(newChild);
-      replaceValues(oldnode.child[i], newnode.child[i]);
-    }
+function createNewTree(oldnode, newnode) {
+  if (!oldnode.child.length) {
+    return;
   }
-
-  return newnode.value = changeValueFB(oldnode.value);
+  for (let i = 0; i < oldnode.child.length; i++) {
+    let newChild = new Node(changeValueFB(oldnode.child[i].value));
+    newnode.child.push(newChild);
+    createNewTree(oldnode.child[i], newnode.child[i]);
+  }
 }
 
-function changeValueFB(value){
-  if ( value % 15 === 0 ){
+function changeValueFB(value) {
+  if (value % 15 === 0) {
     return 'FizzBuzz';
-  } else if ( value % 3 === 0 ){
+  } else if (value % 3 === 0) {
     return 'Fizz';
-  } else if ( value % 5 === 0 ){
+  } else if (value % 5 === 0) {
     return 'Buzz';
   } else {
     return value.toString();
